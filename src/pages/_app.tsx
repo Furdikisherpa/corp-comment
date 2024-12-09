5ba16677a94a90196bbbb125bc2063c4885dd2cf
FurdikiSherpa <118099799+Furdikisherpa@users.noreply.github.com>
2024-12-09 08:26:18 +0545

    favicon added and title changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from 'next/font/google';
+import Head from "next/head";
 
 const inter = Inter({
   subsets: ['latin']
@@ -9,6 +10,9 @@ const inter = Inter({
 export default function App({ Component, pageProps }: AppProps) {
   return (
   <main className={inter.className}>
+    <Head>
+      <title>Corp-comment</title>
+    </Head>
   <Component {...pageProps} />
   </main>
   )
