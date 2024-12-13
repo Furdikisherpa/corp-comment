cda6b5cbf98cbe8d0ea76443e97b2776e20c4130
FurdikiSherpa <118099799+Furdikisherpa@users.noreply.github.com>
2024-12-13 08:13:49 +0545

    styles added

--- src/components/HastagList.tsx
+++ src/components/HastagList.tsx
@@ -1,22 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function HastagList() {
   return (
-      <div className='hastags'>
-        <ul>
-          <li>
-            <button>#Facebook</button>
-          </li>
-          <li>
-            <button>#LinkedIn</button>
-          </li>
-          <li>
-            <button>#KFC</button>
-          </li>
-          <li>
-            <button>#MomoStation</button>
-          </li>
-        </ul>
-      </div>
-  )
+    <ul className="hashtags">
+      <li>
+        <button>#Facebook</button>
+      </li>
+      <li>
+        <button>#LinkedIn</button>
+      </li>
+      <li>
+        <button>#KFC</button>
+      </li>
+      <li>
+        <button>#MomoStation</button>
+      </li>
+    </ul>
+  );
 }
